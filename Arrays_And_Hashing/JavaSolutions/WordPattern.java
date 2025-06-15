0fa0911906f92df8d0f174df601af1321715515e
Philipst77 <philip.stavrev7@gmail.com>
2025-06-15 18:56:58 -0400

    WordPattern LeetCode 290 python and java solution

---
+++ Arrays_And_Hashing/JavaSolutions/WordPattern.java
@@ -0,0 +1,32 @@
+package Arrays_And_Hashing.JavaSolutions;
+
+import java.util.*;
+
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+
+        if (pattern.length() != words.length) {
+            return false;
+        }
+
+        Map<Character, String> charToWord = new HashMap<>();
+        Map<String, Character> wordToChar = new HashMap<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+            String word = words[i];
+
+            if (!charToWord.containsKey(c) && !wordToChar.containsKey(word)) {
+                charToWord.put(c, word);
+                wordToChar.put(word, c);
+            } else {
+                if (!word.equals(charToWord.get(c)) || wordToChar.get(word) != c) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
---
+++ Arrays_And_Hashing/PythonSolutions/WordPattern.py
@@ -0,0 +1,15 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        
+        char_to_word={}
+        word_to_char={}
+        words = s.split()
+
+        for char,word in zip(pattern,words):
+            if char not in char_to_word and word not in word_to_char:
+                char_to_word[char] = word
+                word_to_char[word] = char
+
+            if char_to_word.get(char)!=word or word_to_char.get(word)!=char:
+                return False
+        return True
